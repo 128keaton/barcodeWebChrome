332671c45666c8cbc8300f1728b80f6fc215f90b
Keaton Burleson <keaton.burleson@me.com>
2016-11-07 20:34:30 -0600

    This should fix some issues with finding the printer list
    Another thing to check is permissions :)

--- popup.js
+++ popup.js
@@ -120,55 +120,56 @@ window.addEventListener('load', function(evt) {
 
     if (request.overrideMimeType) request.overrideMimeType(contentType);
 
+    request.onload = function(e) {
+        if (request.readyState === 4 && request.status === 200) {
+
+            lines = request.responseText.split('\n')
+
+            for (var i = 0; i < lines.length; i++) {
+                var d = lines[i];
+                selectForm.options.add(new Option(d, d))
+            }
+
+            if (window.location.hash) {
+                var hash = window.location.hash.substring(1); //Puts hash in variable, and removes the # character
+                document.getElementById('sku').value = hash;
+                hash = true;
+            } else {
+
+                chrome.tabs.executeScript({
+                    code: "window.getSelection().toString();"
+                }, function(selection) {
+                    document.getElementById('sku').value = selection[0];
+                });
+            }
+            for (var i = 0; i < selectForm.options.length; i++) {
+                var d = selectForm.options[i];
+
+
+                if (getCookie("printer") == d.text) {
+                    console.log("cookie printer " + d.text);
+                    selectForm.selectedIndex = i;
+                }
+
+
+
+            }
+
+
+        } else {
+            changeTagName(selectForm, 'img');
+            document.getElementById("button").disabled = true;
+        }
+    };
+
     // exception handling
     try {
         request.send(null);
     } catch (e) {
         return null;
     }
-    if (request.status == 500 || request.status == 404 || request.status == 2 || (request.status == 0 && request.responseText == '')){
-      changeTagName(selectForm, 'img');
-      document.getElementById("button").disabled = true;
-    }
 
 
-
-    lines = request.responseText.split('\n')
-
-
-    for (var i = 0; i < lines.length; i++) {
-        var d = lines[i];
-
-        selectForm.options.add(new Option(d, d))
-
-
-    }
-
-
-    if (window.location.hash) {
-        var hash = window.location.hash.substring(1); //Puts hash in variable, and removes the # character
-        document.getElementById('sku').value = hash;
-        hash = true;
-    } else {
-
-        chrome.tabs.executeScript({
-            code: "window.getSelection().toString();"
-        }, function(selection) {
-            document.getElementById('sku').value = selection[0];
-        });
-    }
-    for (var i = 0; i < selectForm.options.length; i++) {
-        var d = selectForm.options[i];
-
-
-        if (getCookie("printer") == d.text) {
-            console.log("cookie printer " + d.text);
-            selectForm.selectedIndex = i;
-        }
-
-
-
-    }
 
 
 
